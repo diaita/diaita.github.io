a468bc6f30bd9374c68f7017aa4f88c7169f3da4
jliu6184 <125538432+jliu6184@users.noreply.github.com>
2023-07-08 03:20:49 -0700

    started working on main page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
+import './Home.css';
+
 function Home() {
 	return (
 		<div className="Home">
-            <p>THIS IS THE HOME PAGE</p>
+			<div className="main-page">
+				<h2 className="header-2">Good Mood, Good Food.</h2>
+				<h3 className="header-3">Personalized plans to help you achieve a healthy, balanced lifestyle.</h3>
+				<a className="download-btn" href="https://arie.cc/">Download Diaita</a>
+			</div>
 		</div>
     );
 }
