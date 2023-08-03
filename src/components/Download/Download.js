143acbd73b2e6cfe2131e1701cb65bbccf770b82
jliu6184 <125538432+jliu6184@users.noreply.github.com>
2023-08-02 22:34:48 -0700

    added download page

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,10 +1,24 @@
 import './Download.css';
-import Footer from "../Footer/Footer.js"
+import Footer from "../Footer/Footer.js";
+import sample from "/Users/jiaju/Desktop/diaita/diaita-website/src/assets/sample-picture.png";
 
 function Download() {
 	return (
 		<div className="Download">
-            DOWNLOAD
+            <div className="download-header">
+                <h2 className="mission-header">Download the App</h2>
+				<h3 className="mission-subheader">Be the first to get updates about the Diaita App.</h3>
+            </div>
+            <div className="download-section">
+                <div className="left-download">
+                    <h2 className="download-header-text">Be the first.</h2>
+                    <p className="download-text">
+                        Sign-up below to get updates about when we launch the Diaita App!
+                    </p>
+                    <a className="download-btn" href="">Sign up here</a>
+                </div>
+                <img className="sample-photo" src={sample} alt="download diaita" />
+            </div>
             <Footer/>
 		</div>
     );
